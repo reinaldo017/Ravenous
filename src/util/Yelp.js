3dd5a7349ed3f8b42daadcccf27ac9c0461e742f
Reinaldo J <reinaldojacome12@gmail.com>
2022-06-12 12:59:20 -0500

    Yelp.search rewritten with async/await

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,31 +1,36 @@
 const apiKey = process.env.REACT_APP_APIKEY
 
 const Yelp = {
-  search (term, location, sortBy) {
-   return fetch(`/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`
+  search: async (term, location, sortBy) => {
+    try {
+      const response = 
+        await fetch(`/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+          headers: {
+            Authorization: `Bearer ${apiKey}`
+          }
+        })
+    
+      const jsonResponse = await response.json()
+
+      if (jsonResponse.error) {
+        return jsonResponse
+      } else if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          adress: business.location.adress1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories.alias,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }))
       }
-    })
-      .then(response => response.json())
-      .then(jsonResponse => {
-        if (jsonResponse.error) {
-          return jsonResponse
-        } else if(jsonResponse.businesses) {
-          return jsonResponse.businesses.map(business => ({
-            id: business.id,
-            imageSrc: business.image_url,
-            name: business.name,
-            adress: business.location.adress1,
-            city: business.location.city,
-            state: business.location.state,
-            zipCode: business.location.zip_code,
-            category: business.categories.alias,
-            rating: business.rating,
-            reviewCount: business.review_count
-          }))
-        }
-      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
